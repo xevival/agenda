2cd6f10c346f5f84ed3b45cf70e99cb111a7ba03
xvalencia <xevival@gmail.com>
2015-04-19 11:25:08 +0200

    Modificacion url

--- class/Menu.php
+++ class/Menu.php
@@ -13,14 +13,14 @@ class Menu {
                           <span class="icon-bar"></span>
                           <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="#">Panel Gestion</a>
+                        <a class="navbar-brand" href="#">Gestion Agenda</a>
                       </div>
                       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav">
-                          <li><a href="#">Usuarios</a></li>
-                          <li><a href="#">Encuestas</a></li>
-        				<li><a href="#">Clientes</a></li>
-                          <li><a href="../config/index.php">Configuracion</a></li>
+                          <li><a href="contactos.php">Contactos</a></li>
+                          <li><a href="#">Buscar</a></li>
+        				<li><a href="#">Emails</a></li>
+                          
                           <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
                             <ul class="dropdown-menu navbar-right" role="menu">
