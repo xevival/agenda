a8f4f6b7cc9f896daecbb526e749fc7bc4ebb699
xvalencia <xevival@gmail.com>
2015-04-19 11:25:35 +0200

    Añadimos fichero

---
+++ actions_ajax.php
@@ -0,0 +1,47 @@
+<?php
+
+include_once 'session.php';
+include_once 'config.php';
+
+$action = $_REQUEST['action'];
+
+switch ($action){
+
+	case 'cerca_contactos':
+
+		$cadena = $_REQUEST['cadena'];
+
+		$sql = 'SELECT
+					CONCAT_WS(" ",v_nombre,v_apellido1,v_apellido2) as value,
+					id_contacto as id
+				FROM
+					contactos
+				WHERE
+					v_nombre LIKE "%'.$cadena.'%"';
+		
+
+		$contactos = GestorBD::tirarSQL($sql);
+		$total     = GestorBD::totalSQL($contactos);
+
+		$data = array();
+
+		for ($i = 0; $i <= $total; $i++) {
+			$json = array();
+			$json['value'] = utf8_encode($contactos[$i]['value']);
+			$json['id'] = $contactos[$i]['id'];
+			$data[] = $json;
+		}
+
+		$data['results'] = $data;
+		header("Content-type: application/json");
+		echo json_encode($data);
+
+		break;
+
+
+
+}
+
+
+
+?>
