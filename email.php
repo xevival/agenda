e5a850dfc8e3b7ed8501e35099ca0e4307b8b977
xvalencia <xevival@gmail.com>
2015-04-26 19:08:48 +0200

    Añadimos css propios

--- email.php
+++ email.php
@@ -14,7 +14,8 @@ $do = $_REQUEST['do'];
         <meta charset="UTF-8">
         <title>Agenda</title>
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-         <link rel="stylesheet" href="css/autosuggest_inquisitor.css">
+        <link rel="stylesheet" href="css/autosuggest_inquisitor.css">
+         <link rel="stylesheet" href="css/estilos.css">
         <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
         <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
