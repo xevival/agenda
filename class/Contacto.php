4e37a210134e4d5bad3783e9c1ab962889c634e6
xvalencia <xevival@gmail.com>
2015-04-26 19:10:22 +0200

    Añadimos nuevas funciones

--- class/Contacto.php
+++ class/Contacto.php
@@ -2,49 +2,78 @@
 
 class Contacto {
     
-    public static function insertarFormaContacto($datos){
-        GestorBD::insertarDatos('formas_contacto', $datos); 
+    public static function insertarContacto($datos){
+        GestorBD::insertarDatos('contactos', $datos); 
     }
     
-    public static function listadoFormasContacto(){
+    public static function listadoContactos($filtro){
         
-        $sql      = 'SELECT * FROM formas_contacto';
-        $formas   = GestorBD::tirarSQL($sql);
+       	$where = '';
+    	
+    	if ( $filtro == 'all' )$where = '';
+    	else $where = ' WHERE c.v_nombre LIKE "'.$filtro.'%"';
+
+        $sql        = '	SELECT 
+        					id_contacto,v_nombre,CONCAT_WS(" ",v_apellido1,v_apellido2) as apellidos,fecha_nacimiento 
+        				FROM 
+        					contactos c '.$where;
         
-        return $formas;
-    }
-    
-    public static function eliminarFormasContacto($id){
-        GestorBD::eliminarRegistro('formas_contacto', 'id_forma_contacto', $id);
-    }
-    
-    public static function obtenerFormasContacto($id){
+        $contactos   = GestorBD::tirarSQL($sql);
         
-        $sql     = 'SELECT * FROM formas_contacto WHERE id_forma_contacto='.$id.' LIMIT 1';
-        $formas  = GestorBD::tirarSQL($sql);
-        return $formas; 
+        return $contactos;
     }
     
-    public static function modificarFormasContacto($id,$nombre){
-        $sql = "UPDATE formas_contacto SET v_forma_contacto ='$nombre' WHERE id_forma_contacto = ".$id;
+    public static function eliminarContacto($id){
+        GestorBD::eliminarRegistro('contactos', 'id_contacto', $id);
+    }
+    
+    public static function obtenerContacto($id){
+        
+        $sql       = 'SELECT
+        				id_contacto,v_nombre,CONCAT_WS(" ",v_apellido1,v_apellido2) as apellidos,fecha_nacimiento 
+        			  FROM 
+        				contactos 
+        			  WHERE 
+        				id_contacto='.$id.' LIMIT 1';
+        
+        $contacto  = GestorBD::tirarSQL($sql);
+        return $contacto; 
+    }
+    
+    public static function modificarContacto($datos){
+        $sql = "UPDATE 
+        			contactos 
+        		SET 
+        			v_nombre ='$datos[1]',
+        			v_apellido1 ='$datos[2]',
+        			v_apellido2 ='$datos[3]',
+        			fecha_nacimiento ='$datos[4]'
+        		WHERE 
+        			id_contacto ='$datos[0]'";
+        
+ 
+        		
         $GLOBALS['con']->query($sql);
     }
+   
+    public static function devuelveEmail($id){
+    	
+    	
+    	$sql = 'SELECT
+    				v_email
+    			FROM
+    				contactos
+    			WHERE
+    				id_contacto = '.$id;
+    	
     
-    public static function selectFormasContacto(){
-        
-        $formas  = self::listadoFormasContacto();
-        
-        $html  = '<select name="formas" class="form-control">';
-        $html .= '<option value="0">---</option>';
-        
-        foreach ($formas as $f){
-            $html .= '<option value='.$f['id_forma_contacto'].'>'.$f['v_forma_contacto'].'</option>';
-        }
-        
-        $html .= '</select>';
-        
-        return $html;
+    	
+    	$email = GestorBD::tirarSQL($sql);
+    	return $email[0]['v_email'];
+    	
     }
+    
+   
 }
 
 ?>
